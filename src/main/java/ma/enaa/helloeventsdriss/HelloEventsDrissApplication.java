1a5b49d4fb9cc9d52115e3a00c6f23df0cc2d3bb
Idriss-Mikdame <mikdameidriss420@gmail.com>
2025-05-21 05:50:58 +0100

    first commit

---
+++ src/main/java/ma/enaa/helloeventsdriss/HelloEventsDrissApplication.java
@@ -0,0 +1,13 @@
+package ma.enaa.helloeventsdriss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloEventsDrissApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloEventsDrissApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/enaa/helloeventsdriss/HelloEventsDrissApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enaa.helloeventsdriss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloEventsDrissApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
